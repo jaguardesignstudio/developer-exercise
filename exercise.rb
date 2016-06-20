ccaa340dffe4b46c038a8cba4995cb05c639b692
Jordan Lawler <jordan.lawler@colorado.edu>
2016-06-20 13:00:29 -0600

    only have punctuation left

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,17 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+
+    str_array = str.split(" ").map do |word|
+      (word.length <= 4) ? word : marklar_print(word)
+    end
+    str_array*" "
+  end
+
+  def self.marklar_print(word)
+    if []
+      
+    (word[0] == word[0].upcase) ? 'Marklar' : 'marklar'
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
