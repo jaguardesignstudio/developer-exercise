b68c6f9ecece15bc88dd507d773a8d4a154550e6
“kasper341” <kasper_341@hotmail.com>
2016-10-29 12:10:16 -0700

    initialize repo

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,33 @@
-class Exercise
+require 'pry-byebug'
 
+class Exercise
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+
   def self.marklar(str)
     # TODO: Implement this method
+    # p str
+    # binding.pry
+
+    words = str.split
+
+    words.each do |word|
+
+        if word.length > 4
+            p "length greater than 4 #{word}"
+            replaced_with_marklar(word)
+        end
+    end
+
+  end
+
+  def self.replaced_with_marklar(word)
+
+      p "reaplced with marklar called #{word}"
+      if word[0]
+
+      end
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
